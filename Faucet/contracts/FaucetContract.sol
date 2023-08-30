e068ee086710fd7d084991a7ed6be3568ffbbbfe
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-08-30 16:18:29 +0400

    Adding receive function

---
+++ Faucet/contracts/FaucetContract.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 <0.9.0;
+
+
+contract Faucet{
+
+    //The receive function is a special function in Solidity that is automatically invoked when a contract receives Ether without any specific function call
+    receive() external payable{}
+}
+
