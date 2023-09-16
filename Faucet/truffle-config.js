a1b3bbdfb94be6a83430f97ee49afc02648aeca6
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-09-16 21:07:47 +0400

    Connect sepolia node to truffle

--- Faucet/truffle-config.js
+++ Faucet/truffle-config.js
@@ -1,9 +1,15 @@
 module.exports = {
+  contracts_build_directory:"./src/ethereum/contracts",
   networks: {
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 7545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
+    },
+    sepolia:{
+      url:process.env.SEPOLIA_NODE_URL_HTTPS,
+      accounts:[process.env.METAMASK_PRIVATE_KEY],
+      chainId:11155111
     }
   },
   compilers: {
